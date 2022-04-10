5df539d11601227a309f22f4962de983ddac2b17
erikswartzdesign <erik.swartz@gmail.com>
2022-04-10 13:58:30 -0600

    init

---
+++ script.js
@@ -0,0 +1,34 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt User To Select Media Stream, pass to video element, then play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+        
+    } catch (error) {
+        // Catch Error Here
+        console.log('Ya fucked this one up, bro', error);
+    }
+}
+
+buttonElement.addEventListener('click', async () => {
+    // Disable the button when clicked
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    button.disabled = false;
+});
+
+selectMediaStream();
+
+
+
+
+
